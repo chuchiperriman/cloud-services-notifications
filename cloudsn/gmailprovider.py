56b72fd1c234ba2c6043a2910ce51aab8172edb7
Jesús Barbero Rodríguez <chuchiperriman@gmail.com>
2009-12-22 20:04:06 +0100

    Unified atom modules and the providers

--- cloudsn/gmailprovider.py
+++ cloudsn/gmailprovider.py
@@ -1,9 +1,11 @@
 from account import AccountData
-from gmailatom import GmailAtom
 from provider import Provider
+from xml.sax.handler import ContentHandler
+from xml import sax
 import config
 import gtk
 import utils
+import urllib2
 
 ICON = "/home/perriman/dev/cloud-services-notifications/data/gmail.png"
 
@@ -52,3 +54,123 @@ class GMailAccount (AccountData):
     
     def activate (self):
         utils.show_url ("http://gmail.google.com")
+
+
+# Auxiliar structure
+class Mail:
+	title=""
+	summary=""
+	author_name=""
+	author_addr=""
+
+# Sax XML Handler
+class MailHandler(ContentHandler):
+	
+	# Tags
+	TAG_FEED = "feed"
+	TAG_FULLCOUNT = "fullcount"
+	TAG_ENTRY = "entry"
+	TAG_TITLE = "title"
+	TAG_SUMMARY = "summary"
+	TAG_AUTHOR = "author"
+	TAG_NAME = "name"
+	TAG_EMAIL = "email"
+	
+	# Path the information
+	PATH_FULLCOUNT = [ TAG_FEED, TAG_FULLCOUNT ]
+	PATH_TITLE = [ TAG_FEED, TAG_ENTRY, TAG_TITLE ]
+	PATH_SUMMARY = [ TAG_FEED, TAG_ENTRY, TAG_SUMMARY ]
+	PATH_AUTHOR_NAME = [ TAG_FEED, TAG_ENTRY, TAG_AUTHOR, TAG_NAME ]
+	PATH_AUTHOR_EMAIL = [ TAG_FEED, TAG_ENTRY, TAG_AUTHOR, TAG_EMAIL ]
+
+	def __init__(self):
+		self.startDocument()
+
+	def startDocument(self):
+		self.entries=list()
+		self.actual=list()
+		self.mail_count="0"
+
+	def startElement( self, name, attrs):
+		# update actual path
+		self.actual.append(name)
+
+		# add a new email to the list
+		if name=="entry":
+			m = Mail()
+			self.entries.append(m)
+
+	def endElement( self, name):
+		# update actual path
+		self.actual.pop()
+
+	def characters( self, content):
+		# New messages count
+		if (self.actual==self.PATH_FULLCOUNT):
+			self.mail_count = self.mail_count+content
+
+		# Message title
+		if (self.actual==self.PATH_TITLE):
+			temp_mail=self.entries.pop()
+			temp_mail.title=temp_mail.title+content
+			self.entries.append(temp_mail)
+
+		# Message summary
+		if (self.actual==self.PATH_SUMMARY):
+			temp_mail=self.entries.pop()
+			temp_mail.summary=temp_mail.summary+content
+			self.entries.append(temp_mail)
+
+		# Message author name
+		if (self.actual==self.PATH_AUTHOR_NAME):
+			temp_mail=self.entries.pop()
+			temp_mail.author_name=temp_mail.author_name+content
+			self.entries.append(temp_mail)
+
+		# Message author email
+		if (self.actual==self.PATH_AUTHOR_EMAIL):
+			temp_mail=self.entries.pop()
+			temp_mail.author_addr=temp_mail.author_addr+content
+			self.entries.append(temp_mail)
+
+	def getUnreadMsgCount(self):
+		return int(self.mail_count)
+
+# The mail class
+class GmailAtom:
+
+	realm = "New mail feed"
+	host = "https://mail.google.com"
+	url = host + "/mail/feed/atom"
+
+	def __init__(self, user, pswd):
+		self.m = MailHandler()
+		# initialize authorization handler
+		auth_handler = urllib2.HTTPBasicAuthHandler()
+		auth_handler.add_password( self.realm, self.host, user, pswd)
+		opener = urllib2.build_opener(auth_handler)
+		urllib2.install_opener(opener)
+
+	def sendRequest(self):
+		return urllib2.urlopen(self.url)
+
+	def refreshInfo(self):
+		# get the page and parse it
+		p = sax.parseString( self.sendRequest().read(), self.m)
+
+	def getUnreadMsgCount(self):
+		return self.m.getUnreadMsgCount()
+
+	def getMsgTitle(self, index):
+		return self.m.entries[index].title
+
+	def getMsgSummary(self, index):
+		return self.m.entries[index].summary
+
+	def getMsgAuthorName(self, index):
+		return self.m.entries[index].author_name
+
+	def getMsgAuthorEmail(self, index):
+		return self.m.entries[index].author_email
+
+		
--- cloudsn/greaderprovider.py
+++ cloudsn/greaderprovider.py
@@ -1,6 +1,10 @@
 from account import AccountData
 from greaderatom import GreaderAtom
 from provider import Provider
+import urllib2
+import re
+import urllib
+import xml.dom.minidom
 import config
 import gtk
 import utils
@@ -44,4 +48,70 @@ class GReaderAccount (AccountData):
     def __init__(self, name):
         AccountData.__init__(self, name, GetGReaderProvider())
     def activate (self):
-        utils.show_url ("http://reader.google.com")  
+        utils.show_url ("http://reader.google.com")
+
+
+class GreaderAtom:
+	
+	login_url = "https://www.google.com/accounts/ServiceLogin"
+	auth_url = "https://www.google.com/accounts/ServiceLoginAuth"
+	reader_url = "https://www.google.com/reader/api/0/unread-count"
+	
+	def __init__(self, user, pswd):
+		self.username = user
+		self.password = pswd
+		# initialize authorization handler
+		_cproc = urllib2.HTTPCookieProcessor()
+		self.opener = urllib2.build_opener(_cproc)
+		urllib2.install_opener(self.opener)
+
+	def sendRequest(self):
+		f = urllib2.urlopen(self.login_url)
+		data = f.read()
+		galx_match_obj = re.search(r'name="GALX"\s*value="([^"]+)"', data, re.IGNORECASE)
+		galx_value = galx_match_obj.group(1) if galx_match_obj.group(1) is not None else ''
+		params = urllib.urlencode({'Email':self.username,
+					'Passwd':self.password,
+					'GALX':galx_value})
+			   
+		f = urllib2.urlopen(self.auth_url, params)
+
+		return self.opener.open(self.reader_url)
+
+	def parseDocument (self, data):
+		self.feeds=list()
+
+		def processObject (ob):
+			for c in ob.getElementsByTagName ("string"):
+				if c.getAttribute("name") == "id":
+					ftype, s, feed = c.childNodes[0].nodeValue.partition("/")
+					self.feeds.append({"type" : ftype,
+							   "feed" : feed})
+					break
+			
+			for c in ob.getElementsByTagName ("number"):
+				if c.getAttribute("name") == "count":
+					self.feeds[-1]["count"] = c.childNodes[0].nodeValue
+					break
+		
+		doc = xml.dom.minidom.parseString(data)
+		elist = doc.childNodes[0].getElementsByTagName("list")[0]
+		for e2 in elist.getElementsByTagName("object"):
+			processObject (e2)
+
+	def refreshInfo(self):
+		self.parseDocument (self.sendRequest().read())
+
+	def getTotalUnread(self):
+		count = 0
+		for feed in self.feeds:
+			if feed["type"] == "user":
+				name = feed["feed"]
+				name = name[name.rfind ("/") + 1:]
+				if name == "reading-list":
+					count = int(feed["count"])
+
+		return count
+
+
+

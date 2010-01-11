8ca79536096aa9e45d530a5588e43453f849a963
Jesús Barbero Rodríguez <chuchiperriman@gmail.com>
2010-01-11 20:02:45 +0100

    A lot of test with facebook.... Anything new... Added pyfacebook lib

--- src/cloudsn/providers/facebookprovider.py
+++ src/cloudsn/providers/facebookprovider.py
@@ -1,25 +1,32 @@
-from facebook import Facebook
+from cloudsn.providers.facebook import Facebook
 
 if __name__ == '__main__':
     api_key = 'd010cb3718b7fae4aa0efbb6e03403a9'
     secret_key = '166ae7fc00234f750943da222cf72558'
 
-    facebook = Facebook(api_key, secret_key)
+    uid = '1147266555'
+    sk = '2.dW3tUmFz3vRTkVIjGMEbNw__.86400.1263322800-1147266555'
+    auth_token = 'b240299d7c6208ce592c94af-1147266555'
+    facebook = Facebook(api_key, secret_key, auth_token)
     facebook.desktop = True
     facebook.auth.createToken()
-
+    facebook.secret = '2fe02f3f91bbb6f1afdf8c23205aa514'
     # Show login window
     # Set popup=True if you want login without navigational elements
-    facebook.login()
-
-    # Login to the window, then press enter
+    #facebook.login()
     print 'After logging in, press enter...'
-    raw_input()
+    #raw_input()
+    #facebook.session_key = session_key
+    # Login to the window, then press enter
+    
 
-    facebook.auth.getSession()
+    #result = facebook.auth.getSession()
     print 'Session Key:   ', facebook.session_key
     print 'Your UID:      ', facebook.uid
-
+    print 'Auth: ', facebook.auth_token
+    print 'Secret: ', facebook.secret
+    facebook.uid = uid
+    facebook.session_key = sk
     temp = facebook.notifications.get()
     print temp
 

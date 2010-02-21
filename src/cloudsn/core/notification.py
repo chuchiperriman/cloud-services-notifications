9cb4408920272843fe5082a910c494c704e58459
Jesús Barbero Rodríguez <chuchiperriman@gmail.com>
2010-02-22 00:23:16 +0100

    Create the notifications queue
    Currently we show only a notification at the same time. When the
    notification is closed, we show the next one

--- src/cloudsn/core/notification.py
+++ src/cloudsn/core/notification.py
@@ -1,29 +1,52 @@
-class NotificationError(Exception): pass
+from cloudsn import logger
 
-def notify (title, message, icon = None):
+notifications = []
+disable = True
+notifying = False
+
+try:
     import pynotify
     if pynotify.init("Cloud Services Notifications"):
-        n = pynotify.Notification(title, message)
-        n.set_urgency(pynotify.URGENCY_LOW)
-        n.set_timeout(8000)
-        
-        #TODO In debian the messages are not shown in a queue
-        #TODO Change it and do it better
-        """
-        try:
-            from cloudsn.core import indicator
-            from cloudsn.ui.indicators.statusicon import StatusIconIndicator
-            ind = indicator.IndicatorManager.get_instance().get_indicator()
-            if isinstance(ind, StatusIconIndicator):
-                print ind.statusIcon
-                n.attach_to_status_icon (ind.statusIcon)
-        except Exception, e:
-            print e
-            pass;
-        """
-        if icon:
-            n.set_icon_from_pixbuf(icon)
-        n.show()
+        disable = False
     else:
-        raise NotificationError ("there was a problem initializing the pynotify module")
+        logger.error("Cannot initialize libnotify")
+except Exception, e:
+    logger.exception ("there was a problem initializing the pynotify module: %s" % (e))
 
+
+def notify_closed_cb (n, data=None):
+    global notifications, notifying
+    
+    notifying = False
+    notifications.remove (n)
+    n = None
+    notify_process()
+
+def notify_process ():
+    global notifications, notifying
+
+    if notifying == True or len(notifications) == 0:
+        return
+        
+    n = notifications[0]
+    n.connect("closed", notify_closed_cb)
+    n.show()
+    
+    notifying= True
+
+def notify (title, message, icon = None):
+    if disable == True:
+        raise NotificationError ("there was a problem initializing the pynotify module")
+        
+    global notifications
+    n = pynotify.Notification(title, message)
+    n.set_urgency(pynotify.URGENCY_LOW)
+    n.set_timeout(8000)
+    
+    if icon:
+        n.set_icon_from_pixbuf(icon)
+
+    notifications.append(n)
+    notify_process()
+
+class NotificationError(Exception): pass

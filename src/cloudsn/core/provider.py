861cfc1b80e7ff1b1f896844718d5a1f33aa1ede
Jesús Barbero Rodríguez <chuchiperriman@gmail.com>
2010-03-26 21:20:27 +0100

    Implemented the new account dialog (needs some love)

--- src/cloudsn/core/provider.py
+++ src/cloudsn/core/provider.py
@@ -19,9 +19,24 @@ class Provider:
         return self.name
     def get_icon (self):
         return self.icon
+    def get_account_data_widget (self, account=None):
+        """ 
+            If account == None is a new account
+            Returns a widget and it will be inserted into the new account dialog
+        """
+        return None
+    def create_account(self, account_name, widget):
+        """
+            Must return the new created account or raise an exception if there
+            is an error validating the data
+        """
+        raise NotImplementedError("The provider must implement this method!!")
+        
+    #TODO Remove the old function
     def create_account_dialog(self, account_name, parent_window):
         """ Returns the new created account"""
         return None
+    #TODO Remove the old function
     def edit_account_dialog(self, acc, parent_window):
         """ Returns True if the account has been changed"""
         return False
--- src/cloudsn/ui/utils.py
+++ src/cloudsn/ui/utils.py
@@ -2,18 +2,23 @@ import gtk
 
 def create_provider_widget(fields):
     table = gtk.Table(len(fields), 2)
+    table.widgets = {}
     i = 0
     for f in fields:
         hbox = gtk.HBox()
         label = gtk.Label(f["label"])
         if f["type"] == "pwd":
-            print 'password'
             entry = gtk.Entry()
             entry.set_visibility(False)
         else:
             entry = gtk.Entry()
-            print 'string'
+
+        entry.set_name(f["label"])
         table.attach(label, 0, 1, i, i+1)
         table.attach(entry, 1, 2, i, i+1)
         i += 1
+        table.widgets[f["label"]] = entry
     return table
+
+def get_widget_by_label(table, label):
+    return table.widgets[label]

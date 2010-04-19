4d9b62aab44e6bc1333fbac1b051450c5a8f6b8d
Jesús Barbero Rodríguez <chuchiperriman@gmail.com>
2010-04-20 01:43:25 +0200

    Closed version 0.5.8 an the debian files for karmic

--- src/cloudsn/const.py
+++ src/cloudsn/const.py
@@ -1,6 +1,6 @@
 #encoding: utf-8
 APP_NAME="cloudsn"
-APP_VERSION="0.5.1"
+APP_VERSION="0.5.8"
 APP_LONG_NAME="Cloud Services Notifications"
 APP_COPYRIGHT="Copyright © 2009-2010 Jesús Barbero Rodríguez"
 APP_DESCRIPTION="Notify when arrive new mails (pop3, gmail, imap), rss, tweets etc in the configured accounts. "

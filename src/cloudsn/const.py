8dbd1905c73c21a7f3319fe02f1cae4294c23641
Jesús Barbero Rodríguez <chuchiperriman@gmail.com>
2010-06-21 01:45:15 +0200

    Open 0.5.21

--- src/cloudsn/const.py
+++ src/cloudsn/const.py
@@ -1,6 +1,6 @@
 #encoding: utf-8
 APP_NAME="cloudsn"
-APP_VERSION="0.5.20"
+APP_VERSION="0.5.21"
 APP_LONG_NAME="Cloud Services Notifications"
 APP_COPYRIGHT="Copyright © 2009-2010 Jesús Barbero Rodríguez"
 APP_DESCRIPTION="Notify when new mail (POP3/IMAP/Gmail), feed items, tweets or items from other cloud services arrive."

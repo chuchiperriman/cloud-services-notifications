cc6f6771d04362932c120733f68b43c912698e94
Jesús Barbero Rodríguez <chuchiperriman@gmail.com>
2010-09-20 19:45:37 +0200

    Added csv test

---
+++ tests/testcsv.py
@@ -0,0 +1,40 @@
+import sys
+import csv
+
+FILE = "/tmp/sample.csv"
+
+def main():
+    
+    feeds = dict()
+    feeds['1'] = ('1','asdfasdfasdf')
+    feeds['2'] = ('2','222222222222')
+    
+    """
+    data = [
+        ("And Now For Something Completely Different", 1971, "Ian MacNaughton"),
+        ("Monty Python And The Holy Grail", 1975, "Terry Gilliam, Terry Jones"),
+        ("Monty Python's Life Of Brian", 1979, "Terry Jones"),
+        ("Monty Python Live At The Hollywood Bowl", 1982, "Terry Hughes"),
+        ("Monty Python's The Meaning Of Life", 1983, "Terry Jones")
+    ]
+    """
+    writer = csv.writer(open(FILE, "a+"), delimiter='\t')
+
+    """
+    for item in data:
+        writer.writerow(item)
+        
+    writer.writerows(data)
+    """
+    writer.writerows(feeds.values())
+        
+    
+    reader = csv.reader(open(FILE), delimiter='\t')
+
+    for feed_id, feed_title in reader:
+        print feed_id, feed_title
+
+if __name__ == '__main__':
+    sys.exit(main())
+
+    

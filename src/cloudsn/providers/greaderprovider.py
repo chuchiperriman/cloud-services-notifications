9ebe1e6130fb3600080df915a0c56a877be8c3b2
Jesús Barbero Rodríguez <chuchiperriman@gmail.com>
2010-09-25 18:22:02 +0200

    Fixed #622748: correct message in greader notifications

--- src/cloudsn/providers/greaderprovider.py
+++ src/cloudsn/providers/greaderprovider.py
@@ -29,32 +29,32 @@ class GReaderProvider(ProviderUtilsBuilder):
         acc = AccountCacheMails(props, self)
         acc.properties["activate_url"] = "http://reader.google.com"
         return acc
-        
+
     def update_account (self, account):
         credentials = account.get_credentials()
         g = GreaderAtom (credentials.username, credentials.password)
         g.refreshInfo()
-        
+
         news = []
         new_count = g.getTotalUnread() - account.total_unread
         if new_count > 1:
-            news.append(Notification('', 
-                _('%d unread news') % (new_count),
+            news.append(Notification('',
+                _('%d new unread news of %i') % (new_count, g.getTotalUnread()),
                 ''))
         elif new_count == 1:
-            news.append(Notification('', 
-                _('%d unread new') % (new_count),
+            news.append(Notification('',
+                _('%d new unread new of %i') % (new_count, g.getTotalUnread()),
                 ''))
-        
+
         account.new_unread = news;
-        
+
         account.total_unread = g.getTotalUnread()
 
     def get_dialog_def (self):
         return [{"label": "User", "type" : "str"},
                 {"label": "Password", "type" : "pwd"},
                 {"label": "Show notifications", "type" : "check"}]
-    
+
     def populate_dialog(self, widget, acc):
         credentials = acc.get_credentials_save()
         self._set_text_value ("User", credentials.username)
@@ -63,16 +63,16 @@ class GReaderProvider(ProviderUtilsBuilder):
             show_notifications = acc["show_notifications"]
         else:
             show_notifications = True
-            
+
         self._set_check_value("Show notifications", show_notifications)
-    
+
     def set_account_data_from_widget(self, account_name, widget, account=None):
         username = self._get_text_value ("User")
         password = self._get_text_value ("Password")
         show_notifications = self._get_check_value("Show notifications")
         if username=='' or password=='':
             raise Exception(_("The user name and the password are mandatory"))
-        
+
         if not account:
             props = {'name' : account_name, 'provider_name' : self.get_name(),
                 'show_notifications' : show_notifications,
@@ -80,18 +80,18 @@ class GReaderProvider(ProviderUtilsBuilder):
             account = self.load_account(props)
         else:
             account["show_notifications"] = show_notifications
-            
+
         credentials = Credentials(username, password)
         account.set_credentials(credentials)
-        
+
         return account
-        
+
 class GreaderAtom:
-	
+
 	login_url = "https://www.google.com/accounts/ServiceLogin"
 	auth_url = "https://www.google.com/accounts/ServiceLoginAuth"
 	reader_url = "https://www.google.com/reader/api/0/unread-count"
-	
+
 	def __init__(self, user, pswd):
 		self.username = user
 		self.password = pswd
@@ -108,7 +108,7 @@ class GreaderAtom:
 		params = urllib.urlencode({'Email':self.username,
 					'Passwd':self.password,
 					'GALX':galx_value})
-			   
+
 		f = urllib2.urlopen(self.auth_url, params)
 
 		return self.opener.open(self.reader_url)
@@ -123,12 +123,12 @@ class GreaderAtom:
 					self.feeds.append({"type" : ftype,
 							   "feed" : feed})
 					break
-			
+
 			for c in ob.getElementsByTagName ("number"):
 				if c.getAttribute("name") == "count":
 					self.feeds[-1]["count"] = c.childNodes[0].nodeValue
 					break
-		
+
 		doc = xml.dom.minidom.parseString(data)
 		elist = doc.childNodes[0].getElementsByTagName("list")[0]
 		for e2 in elist.getElementsByTagName("object"):
@@ -147,6 +147,4 @@ class GreaderAtom:
 					count = int(feed["count"])
 
 		return count
-
-
 
